61a8f46a129846c4f6435f7dec048c2796df09a7
kravchuks <kravchuk.slv@rambler.ru>
2023-11-29 23:00:15 -0600

    Create a menu and fix issues

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { UisHouseUser } from '@iconscout/react-unicons-solid'
 import "./header.css";
-
 
 const Header = () => {
   return (
@@ -14,35 +12,47 @@ const Header = () => {
         <div className="nav__menu">
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <UisHouseUser className=" nav__icon"/>Home
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav__icon" />Home
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i>About
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i>Skills
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i>Services
               </a>
             </li>
 
             <li className="nav__item">
-              <a href="" className="nav__link">
-                <i className="uil uil- nav__icon"></i>
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i>Portfolio
+              </a>
+            </li>
+
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i>Contact
               </a>
             </li>
           </ul>
+
+          <i className="uil uil-times nav__close"></i>
+        </div>
+
+        <div className="nav__togle">
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
